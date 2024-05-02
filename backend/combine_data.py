bf51184762678063e75412fe3be32cfca2551b3d
Anton <who@knows.where>
2024-05-02 20:05:16 +0200

    Reduce API calls by downloading avatars directly

--- backend/combine_data.py
+++ backend/combine_data.py
@@ -1,19 +1,16 @@
 from .functions import combine_json_files
 from .json_into_html import generate_html_from_json
 
+
 def combine_data():
     report_json_directory = "./reports-json/"
 
-    # Specify the folder containing JSON files and the output file name
-    json_folder_path = './reports-json/'
-    html_folder_path = './reports-html/'
-    json_output_file = json_folder_path + '_combined_data.json'
-    html_output_file = html_folder_path + '_combined_data.html'
+def combine_data(report_json_directory="./reports-json/", report_html_directory="./reports-html/"):
 
-    combine_json_files(json_folder_path, json_output_file)
+    combine_json_files(report_json_directory, f"{report_json_directory}_combined_data.json")
 
     # Generate the HTML file from JSON
-    generate_html_from_json(json_output_file, html_output_file)
+    generate_html_from_json(f"{report_json_directory}_combined_data.json", f"{report_html_directory}_combined_data.html")
 
 # Call the function to execute the functionality
 if __name__ == "__main__":
--- backend/functions.py
+++ backend/functions.py
@@ -1,14 +1,19 @@
-from math import radians, pi, cos
-from os import path, listdir
-from json import load, dump
+from concurrent.futures import ThreadPoolExecutor
+from json import dump, load
+from math import cos, pi, radians
+from os import listdir, path
+from time import sleep
+
+from bs4 import BeautifulSoup
 from requests import get
+
 
 def generate_pattern(length):
     pattern = []
     pattern.extend([1, 1, 1])  # Start with three occurrences of 1
     for i in range(2, (length - 4) // 2):
         pattern.extend([i, i])
-    pattern.append((length - 3) // 2 - 1) 
+    pattern.append((length - 3) // 2 - 1)
     return pattern
 
 def calculate_length(metres):
@@ -17,7 +22,7 @@ def calculate_length(metres):
         return metres
     else:
         return (metres - remainder + 800)
-    
+
 # Function to calculate coordinates based on steps and direction
 def calculate_coordinates(lat, lon, direction, distance):
     # Radius of the Earth in kilometers
@@ -45,14 +50,6 @@ def calculate_coordinates(lat, lon, direction, distance):
         raise ValueError("Invalid direction")
     return new_lat, new_lon
 
-# Function to load existing data from file
-def load_existing_data(filename):
-    existing_data = {}
-    if path.exists(filename):
-        with open(filename, 'r') as file:
-            existing_data = load(file)
-    return existing_data
-
 # Function to combine all json files into one
 def combine_json_files(folder_path, output_file):
     combined_data = {}
@@ -62,7 +59,7 @@ def combine_json_files(folder_path, output_file):
             with open(file_path, 'r') as file:
                 data = load(file)
                 combined_data.update(data)  # Merge dictionaries
-    
+
     with open(output_file, 'w') as output:
         dump(combined_data, output, indent=4)
 
@@ -78,3 +75,51 @@ def get_location_details(latitude, longitude):
         return town, city, country
     else:
         return None, None, None
+
+def countdown_timer(duration):
+    for remaining in range(duration, 0, -1):
+        print(f"\t\t[ > ] Sleeping for {remaining} seconds before processing the next coordinates...", end="\r")
+        sleep(1)
+    print(" " * 100, end="\r")  # Clear the line after countdown
+
+#Download avatars
+def download_avatar(user_id, username, user_url, output_folder):
+    avatar_filename = path.join(output_folder, f"{user_id}-{username}.jpg")
+    if username is None:
+        return
+
+    if path.exists(avatar_filename):
+        print(f"Avatar for user {user_id}-{username} already exists. Skipping download.")
+        return
+
+    try:
+        response = get(user_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            meta_tag = soup.find("meta", property="og:image")
+            if meta_tag and "cdn-telegram.org" in meta_tag["content"]:
+                image_url = meta_tag["content"]
+                response = get(image_url)
+                if response.status_code == 200:
+                    with open(avatar_filename, 'wb') as image_file:
+                        image_file.write(response.content)
+                    print(f"Downloaded avatar for user {user_id}-{username} successfully")
+                else:
+                    print(f"Failed to download avatar for user {user_id}-{username}. Status code: {response.status_code}")
+            else:
+                print(f"No profile photo found for user {user_id}-{username}")
+        else:
+            print(f"Failed to fetch user page for user {user_id}-{username}. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error downloading avatar for user {user_id}-{username}: {e}")
+
+def download_avatars(json_file, output_folder):
+    print(f"Starting avatars download based on {json_file}...")
+    with open(json_file, 'r') as f:
+        data = load(f)
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for user_id, user_data in data.items():
+            username = user_data.get('username', '')
+            user_url = f"https://t.me/{username}"
+            executor.submit(download_avatar, user_id, username, user_url, output_folder)
